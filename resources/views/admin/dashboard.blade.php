9cc1d8671827ce73814679d4a03c3a26b7ba1a13
Sushil Chand <sushilchand662@gmail.com>
2024-02-20 23:20:32 +0545

    Admin-dashboard add user

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,7 +21,7 @@
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item active">Operator Dashboard</li>
-              jfjdjdkg
+              
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
