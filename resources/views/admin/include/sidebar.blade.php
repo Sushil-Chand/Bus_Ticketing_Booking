08a676ac44a2adea114529a3d63f71fb3ceccaed
Sushil Chand <sushilchand662@gmail.com>
2024-02-20 23:19:53 +0545

    add user route  optimize

--- resources/views/admin/include/sidebar.blade.php
+++ resources/views/admin/include/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <br>
     <!-- Brand Logo -->
-    <a href="/dashboard" class="brand-link">
+    <a href="/admin-dashboard" class="brand-link">
         <img src="dist/img/busphoto.jpg" alt="admin online system Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <span class="brand-text font-weight-light">Admin-System</span>
@@ -54,7 +54,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <a href="{{route('user.index')}}" class="nav-link">
                         <i class="nav-icon fas fa-user"></i>
                         <p>
                             User
@@ -101,6 +101,6 @@
     </div>
     <!-- /.sidebar -->
 </aside>
-
+</
 
 
