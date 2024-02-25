58663634a782f479c5716165e16cf4e19515a445
Sushil Chand <sushilchand662@gmail.com>
2024-02-25 23:16:39 +0545

    make the driver model

---
+++ app/Models/Operator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Operator extends Model
+{
+    use HasFactory;
+    protected $table = 'operators';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'logo',
+        'status',
+    ];
+}
