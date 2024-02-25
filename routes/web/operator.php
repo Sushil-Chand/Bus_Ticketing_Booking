07638b971aa75cdf881f72a9f1e8862c85de902e
Sushil Chand <sushilchand662@gmail.com>
2024-02-25 23:13:08 +0545

    Add the route operator

---
+++ routes/web/operator.php
@@ -0,0 +1,14 @@
+<?php
+
+
+use App\Http\Controllers\OperatorController;
+use Illuminate\Support\Facades\Route;
+
+// Operator Routes
+        Route::get('/operators', [OperatorController::class, 'index'])->name('operators.index');
+        Route::get('/operators/create', [OperatorController::class, 'create'])->name('operators.create');
+        Route::post('/operators/store', [OperatorController::class, 'store'])->name('operators.store');
+        Route::get('/operators/{id}', [OperatorController::class, 'show'])->name('operators.show');
+        Route::get('/operators/{id}/edit', [OperatorController::class, 'edit'])->name('operators.edit');
+        Route::put('/operators/{id}', [OperatorController::class, 'update'])->name('operators.update');
+        Route::delete('/operators/{id}', [OperatorController::class, 'destroy'])->name('operators.destroy');
