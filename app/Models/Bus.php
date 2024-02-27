eca32f078fdafbe9b4fbaf099319b4bd824b69a0
Sushil Chand <sushilchand662@gmail.com>
2024-02-27 23:52:00 +0545

    create bus model

---
+++ app/Models/Bus.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bus extends Model
+{
+    use HasFactory;
+    protected $table = 'buses';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'bus_name',
+        'bus_code',
+        'type',
+        'operator_id',
+        'total_seats',
+        'status',
+        'user_id',
+        'driver_id',
+        'status'
+    ];
+
+    public function operator()
+    {
+        return $this->belongsTo(Operator::class, 'operator_id');
+    }
+
+    public function driver()
+    {
+        return $this->belongsTo(Driver::class, 'driver_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
