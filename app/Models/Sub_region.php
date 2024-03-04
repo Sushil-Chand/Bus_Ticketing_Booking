3482dd9f60fc99206bf2eec2f7ec424e5df5c9d2
Sushil Chand <sushilchand662@gmail.com>
2024-03-05 00:15:01 +0545

    make the sub_region model

---
+++ app/Models/Sub_region.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sub_region extends Model
+{
+    use HasFactory;
+    protected $table = 'sub_regions';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'sub_region_name',
+        'sub_region_code',
+        'region_id',
+        'status',
+    ];
+
+}
