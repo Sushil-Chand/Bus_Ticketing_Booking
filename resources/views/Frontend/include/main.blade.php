45d52a05dcb92843cb7e304615239dd462932791
Sushil Chand <sushilchand662@gmail.com>
2024-03-18 19:54:47 +0545

    add the fronted part

---
+++ resources/views/Frontend/include/main.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  @include('Frontend.include.head')
+ 
+  <body>
+    {{-- <div class="wrapper"> --}}
+
+        {{-- <body class="hold-transition sidebar-mini layout-fixed">
+            <div class="wrapper"> --}}
+    
+        
+      <!-- Navbar -->
+      @include('Frontend.include.navbar')
+
+      <!-- Main sidebar -->
+      {{-- @include('Frontend.include.sidebar') --}}
+
+      <!-- Main content -->
+      <section class="content">
+        {{-- @include('sweetalert::alert') --}}
+        @yield('content')
+      
+      </section>
+      <!-- /.content -->
+
+      <!-- Main Footer -->
+      @include('Frontend.include.footer')
+
+      <!-- Scripts -->
+      @include('Frontend.include.script')
+      
+    </div>
+    <!-- ./wrapper -->
+  </body>
+
+</html>
