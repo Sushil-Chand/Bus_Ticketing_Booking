309038dc222c043820edb23bb28129c340c7ab15
Sushil Chand <sushilchand662@gmail.com>
2024-03-25 23:42:46 +0545

    frontend UI change

--- resources/views/Frontend/include/head.blade.php
+++ resources/views/Frontend/include/head.blade.php
@@ -6,7 +6,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!--font-family-->
-    <link href="{{asset('https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i')}}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     
     <!-- title of site -->
     <title>Online bus booking</title>
@@ -31,7 +31,7 @@
     <link rel="stylesheet" href="{{asset('frontend/assets/css/slick-theme.css')}}">
     
     <!--bootstrap.min.css-->
-    <link rel="stylesheet" href="{{asset('frontendassets/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.min.css')}}">
     
     <!-- bootsnav -->
     <link rel="stylesheet" href="{{asset('frontend/assets/css/bootsnav.css')}}" >	
@@ -40,7 +40,9 @@
     <link rel="stylesheet" href="{{asset('frontend/assets/css/style.css')}}">
     
     <!--responsive.css-->
-    <link rel="stylesheet" href="{{asset('frontendassets/css/responsive.css')}}">
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/responsive.css')}}">
+
+    <link href="{{asset('frontend/assets/bootstrap-5.3.3-dist/css/bootstrap.css')}}" rel="stylesheet">
     
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -49,6 +51,6 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css')}}">
+        <link href="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH')}}" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css')}}">
 </head>
--- resources/views/Frontend/include/script.blade.php
+++ resources/views/Frontend/include/script.blade.php
@@ -24,8 +24,5 @@
     
     <!--Custom JS-->
     <script src="{{asset('frontend/assets/js/custom.js')}}"></script>
-    <script src="{{asset('https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js')}}"></script>
-    <script src="{{asset('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js')}}" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" ></script>
-    <script src="{{asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js')}}"></script> 
+    <script src="{{asset('frontend/assets/bootstrap-5.3.3-dist/js/bootstrap.js')}}"></script>
     
