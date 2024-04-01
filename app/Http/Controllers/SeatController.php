6bd56b6005841403181c13e0a8b44768e09a8c9a
Sushil Chand <sushilchand662@gmail.com>
2024-04-01 23:41:51 +0545

    optimize the seatController

---
+++ app/Http/Controllers/SeatController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Bus_Schedule;
+use App\Models\Bus;
+use App\Models\Seat;
+use Illuminate\Http\Request;
+
+class SeatController extends Controller
+{
+    public function index()
+    {   
+        
+        $busSchedules = Bus_Schedule::all();
+        $buses = [];
+
+        foreach ($busSchedules as $busSchedule) {
+            $busId = $busSchedule->bus_id;
+            $bus = Bus::find($busId);
+    
+            if ($bus) {
+                $buses[] = $bus;
+            }
+        }
+    
+        // dd($buses);
+         return view('admin.Seat.index',compact('buses','busSchedules'));
+    }
+
+
+
+
+
+
+    public function viewseats($id)
+   {
+
+    $bus= Bus::findOrFail($id);
+   
+
+    return view('admin.Seat.viewseat',compact('bus'));
+   }
+}
